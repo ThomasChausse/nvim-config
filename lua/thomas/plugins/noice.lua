3e974541b4c5fc6803699fe8f2a81f134172a6f5
Thomas <thomas.chausse.1@ens.etsmtl.ca>
2025-04-10 23:32:28 -0400

    new feature toggleterm, fix cmdline flicker on mv line

--- lua/thomas/plugins/noice.lua
+++ lua/thomas/plugins/noice.lua
@@ -1,19 +1,45 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		routes = {
-			{
-				filter = { event = "notify", find = "No information available" },
-				opts = { skip = true },
-			},
-		},
-		presets = {
-			lsp_doc_border = true,
-		},
-	},
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    routes = {
+      {
+        filter = { event = "notify", find = "No information available" },
+        opts = { skip = true },
+      },
+    },
+    presets = {
+      lsp_doc_border = true,
+    },
+    -- Custom message settings
+    messages = {
+      enabled = true,
+      timeout = 1000,
+      view = "mini", -- Use nvim-notify for notifications
+      view_error = "notify",
+      view_warning = "mini",
+    },
+    -- Diagnostic configuration
+    lsp = {
+      progress = { enabled = false },  -- Optionally disable LSP progress messages
+      signature = { enabled = false }, -- Disable signature help popups
+    },
+    -- Customize notifications
+    notify = {
+      level = "error",    -- Only show error-level notifications
+      timeout = 3000,     -- Notification timeout duration
+      render = "compact", -- Compact render for notifications
+      view = "notify",
+    },
+
+    cmdline = {
+      enable = true,
+      view = "cmdline_popup"
+
+    }
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
 }
